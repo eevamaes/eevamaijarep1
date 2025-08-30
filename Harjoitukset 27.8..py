e600e82841ab4b6b1e056d13b8f495e2844e2967
eevamaes <eevamaes@metropolia.fi>
2025-08-30 06:44:16 +0300

    moi

--- Harjoitukset 27.8..py
+++ Harjoitukset 27.8..py
@@ -1,4 +1,8 @@
-nimi=input ("Anna lemmikkisi nimi:")
-onimi=input("Anna oma nimesi:")
-print(nimi+onimi)
-print("Heippa!"+nimi+"ja"+onimi)
+nimi=input("Mika on nimesi?")
+print("Hyvaa huomenta, "+nimi+"!")
+
+
+
+
+
+
