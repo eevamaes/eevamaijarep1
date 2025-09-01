943adf40cee337ace8b237e1d28a089b64295fe9
eevamaes <eevamaes@metropolia.fi>
2025-09-01 17:02:22 +0300

    moi

---
+++ Moduuli02_tehtavat/tehtava6.py
@@ -0,0 +1,17 @@
+#Kirjoita ohjelma, joka arpoo ja tulostaa kaksi erilaista numerolukon koodia:
+#kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9
+
+import random
+luku1=int(random.randint(0,9))
+luku2=int(random.randint(0,9))
+luku3=int(random.randint(0,9))
+print(luku1,luku2,luku3)
+
+#nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6
+a=int(random.randint(1,6))
+b=int(random.randint(1,6))
+c=int(random.randint(1,6))
+d=int(random.randint(1,6))
+print(a,b,c,d)
+
+
