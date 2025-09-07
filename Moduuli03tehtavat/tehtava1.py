a71a60567437567b2ec173cf9914762f81b6a4de
eevamaes <eevamaes@metropolia.fi>
2025-09-07 11:32:48 +0300

    moi

---
+++ Moduuli03tehtavat/tehtava1.py
@@ -0,0 +1,7 @@
+luku1 = float(input("Montako senttimetriä pyytämäsi kuhan pituus on?"))
+
+if luku1 < 37:
+    print(("Sinun pitää päästää kuha takaisin veteen."), ("Pyytämäsi kuha on"), (37-luku1),
+     ("cm alamittainen."))
+else:
+    print("Voit pitää pyydystämäsi kuhan.")
