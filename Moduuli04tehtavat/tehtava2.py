fa945f863f397b88bbdda70a437ceb4a4295a759
eevamaes <eevamaes@metropolia.fi>
2025-09-27 10:20:51 +0300

    moi

---
+++ Moduuli04tehtavat/tehtava2.py
@@ -0,0 +1,21 @@
+#Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä
+# antaa negatiivisen tuumamäärän. Sen jälkeen ohjelma lopettaa toimintansa.
+# 1 tuuma = 2,54 cm
+
+
+while True:
+    luku = int(input("Anna tuumamäärä."))
+    print (luku *2.54)
+    if luku <=0:
+        break
+
+
+
+
+
+
+
+
+
+
+
