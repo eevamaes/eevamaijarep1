b160f1fc39fd43ba65fd679933c1bf94f9c21d67
eevamaes <eevamaes@metropolia.fi>
2025-09-28 16:56:20 +0300

    moi

--- Moduuli04tehtavat/tehtava2.py
+++ Moduuli04tehtavat/tehtava2.py
@@ -1,8 +1,3 @@
-#Kirjoita ohjelma, joka muuntaa tuumia senttimetreiksi niin kauan kunnes käyttäjä
-# antaa negatiivisen tuumamäärän. Sen jälkeen ohjelma lopettaa toimintansa.
-# 1 tuuma = 2,54 cm
-
-
 while True:
     luku = int(input("Anna tuumamäärä."))
     print (luku *2.54)
---
+++ Moduuli04tehtavat/tehtava3.py
@@ -0,0 +1,60 @@
+#En saa ohjelmaa tulostamaan pienintä ja suurinta lukua.
+
+luku = input("Anna jokin luku, kiitos.")
+numero = int(luku)
+min(numero,numero)
+max(numero,numero)
+
+while True:
+    numero=int(luku)
+
+    luku = input("Anna jokin luku, kiitos.")
+
+    if luku == "":
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
