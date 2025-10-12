c66c793cc1e966847c2890d50f97cab6d3c34e1c
eevamaes <eevamaes@metropolia.fi>
2025-10-12 14:59:34 +0300

    moi

--- Moduuli05tehtavat/tehtava3.py
+++ Moduuli05tehtavat/tehtava3.py
@@ -1,1 +1,12 @@
-luku=int(input("Anna jokin luku."))
+luku= int(input("Anna jokin luku."))
+for numero in range (2,luku):
+
+        if luku % numero ==0:
+            print("Tämä ei ole alkuluku")
+            break
+else:
+    print("Tämä on alkuluku. JEE!")
+
+
+
+
